c5dcf50ae5b9010b77a9169fcc0a587477f693de
Mahen-4 <mahen.ath@outlook.com>
2024-01-28 20:00:16 +0100

    [ADDED] test e2e

---
+++ tests_e2e/example.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test.describe("MSG Tests", ()=>{
+  test('Err MSG', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    await page.getByPlaceholder('Rechercher une ville').click();
+    await page.getByPlaceholder('Rechercher une ville').fill('Napolie');
+    await page.locator('form').getByRole('button').click();
+    await expect(page.getByText('Ville / Pays Invalide')).toBeVisible();
+  });
+  
+  test('Toaster MSG', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    await page.locator('h1 > div > svg').click();
+    await expect(page.getByRole('status')).toBeVisible();
+  });  
+})
+
+
