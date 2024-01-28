9a7e7cecc9f6f37e7d43319595b72c9da91677d3
Mahen-4 <mahen.ath@outlook.com>
2024-01-28 20:03:10 +0100

    [ADDED] test e2e

--- tests_e2e/example.spec.ts
+++ tests_e2e/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe("MSG Tests", ()=>{
   test('Err MSG', async ({ page }) => {
-    await page.goto('http://localhost:3000/');
+    await page.goto('/');
     await page.getByPlaceholder('Rechercher une ville').click();
     await page.getByPlaceholder('Rechercher une ville').fill('Napolie');
     await page.locator('form').getByRole('button').click();
@@ -10,7 +10,7 @@ test.describe("MSG Tests", ()=>{
   });
   
   test('Toaster MSG', async ({ page }) => {
-    await page.goto('http://localhost:3000/');
+    await page.goto('/');
     await page.locator('h1 > div > svg').click();
     await expect(page.getByRole('status')).toBeVisible();
   });  
